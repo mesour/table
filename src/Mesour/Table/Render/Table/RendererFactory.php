141fa380991327fd28d035cbcc82d2297d07f096
mesour <matous.nemec@gmail.com>
2017-09-25 11:00:41 +0200

    Updated year and composer.json

--- src/Mesour/Table/Render/Table/RendererFactory.php
+++ src/Mesour/Table/Render/Table/RendererFactory.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the Mesour Table (http://components.mesour.com/component/table)
  *
- * Copyright (c) 2015 Matouš Němec (http://mesour.com)
+ * Copyright (c) 2017 Matouš Němec (http://mesour.com)
  *
  * For full licence and copyright please view the file licence.md in root of this project
  */
